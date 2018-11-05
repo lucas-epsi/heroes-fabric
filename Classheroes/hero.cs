949a7543d1de70076bbfab8a93d84dd5136b19f5
FLORENTIN Lucas 1 <lucas.florentin1@epsi.fr>
2018-11-05 13:37:17 +0100

    finalisation de la sauvegarde et commencement de la phase de battle

--- Classheroes/hero.cs
+++ Classheroes/hero.cs
@@ -13,20 +13,37 @@ namespace Classheroes
 {
     [Serializable()]
     public class heroes
-    {   
-
+    {
+        public string auberge { get; set; }
         public string name { get; set; }
         public int niveau { get; set; }
         public int xp { get; set; }
         public int PV { get; set; }
+        public int force { get; set; }
+        public int endurance { get; set; }
+        public int agilité { get; set; }
+
+
 
         public heroes()
             {
-
             niveau = 1;
             xp = 0;
             PV = 15;
+            force = 10;
+            endurance = 10;
+            agilité = 10;
              }
+
+         public int GetDegat()
+        {
+            int Degat;
+            Random aleatoire = new Random();
+            int entre1et9 = aleatoire.Next(10);
+            Degat = force + (force+entre1et9) / 10;
+            return Degat;
+        }
+
     }
 }
 
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,25 +14,32 @@ namespace herofabric
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("     BIENVENUE DANS L'AUBERGE DES HEROS      ");
+            
             List<heroes> personnages = new List<heroes>();
-
-
+            string auberge;
+            
             string actualpath = Directory.GetCurrentDirectory();
-            Console.WriteLine(actualpath);
             if (File.Exists(actualpath + "/sauvegarde.sav"))
             {   
                 Console.WriteLine("récupération Sauvegarde");
                 XmlSerializer reader = new XmlSerializer(typeof(List<heroes>));
                 StreamReader file = new StreamReader("sauvegarde.sav");
                 personnages = (List<heroes>)reader.Deserialize(file);
+                auberge = personnages[1].auberge;
                 file.Close();
-                Console.WriteLine("Sauvegarde récupérée");
+                Console.WriteLine("Sauvegarde récupérée \n");
+                Console.ReadKey();
+                Console.Clear();
+            } else {
+                Console.WriteLine("Entrez le nom de votre Auberge : ");
+                auberge = Console.ReadLine();
             }
             int Choix = 0;
             // beginning of the loop for the iteration of the interface
             do
             {
-                Console.WriteLine("     AUBERGE DES HEROS       ");
+                Console.WriteLine("     "+auberge+"\n");
                 Console.WriteLine("     choisissez action       ");
                 Console.WriteLine("1) Créer nouvel héro ");
                 Console.WriteLine("2) Supprimer héro existant ");
@@ -42,10 +49,11 @@ namespace herofabric
                 Console.WriteLine("6) Quitter Jeu \n");
 
 
-                Thread.Sleep(1000);
+                Console.ReadKey();
 
                 Console.WriteLine("Entrez Séléction : ");
-                Choix = Convert.ToInt32(Console.ReadLine());
+                try { Choix = Convert.ToInt32(Console.ReadLine()); }
+                catch(FormatException) { Console.WriteLine("try again \n"); }
                 Console.Clear();
 
                 switch (Choix)
@@ -57,10 +65,11 @@ namespace herofabric
                         nom = Console.ReadLine();
                         heroes newhero1 = new heroes();
                         newhero1.name = nom;
+                        newhero1.auberge = auberge;
 
                         personnages.Add(newhero1);
                         methods.retourchariot();
-                        
+
                         Console.WriteLine("nom héro : " + newhero1.name);
                         Console.WriteLine("Niveau héro : " + newhero1.niveau);
                         Console.WriteLine("XP héro : " + newhero1.xp);
@@ -70,38 +79,58 @@ namespace herofabric
                         StreamWriter ecrivain = new StreamWriter("sauvegarde.sav");
                         serializer.Serialize(ecrivain, personnages);
                         ecrivain.Close();
-                       
-                        Thread.Sleep(1000);
+
+                        Console.ReadKey();
 
                         break;
 
                     case 2:
+
+                        break;
+
+                    case 3:
                         Console.WriteLine("Liste des personnages créés : \n");
-                        foreach(heroes cpt in personnages)
+                        foreach (heroes cpt in personnages)
                         {
-                            Console.WriteLine("nom : "+cpt.name);
-                            Console.WriteLine("Niveau : "+cpt.niveau);
+                            Console.WriteLine("nom : " + cpt.name);
+                            Console.WriteLine("Niveau : " + cpt.niveau);
                             Console.WriteLine("XP : " + cpt.xp);
                             Console.WriteLine("PV : " + cpt.PV + "\n");
                         }
-                        Thread.Sleep(3000);
-                        break;
-                        
-                    case 3:
-                        XMLwrite read = new XMLwrite();
-                        read.ReadXML();
+                        Console.ReadKey();
                         break;
 
                     case 4:
 
+                        string choixperso;
+                        int index = 0;
+                        Console.WriteLine("Sélectionner héro :");
+                        foreach (heroes perso in personnages)
+                        {
+                            Console.WriteLine("nom :" + perso.name);
+                        }
+                        choixperso = Console.ReadLine();
+
+                        foreach (heroes perso in personnages)
+                        {
+                            if (choixperso == perso.name)
+                            {
+                                Console.WriteLine("héro sélectionné : " + perso.name);
+                                index = personnages.IndexOf(perso);
+                            }
+                        }
+
+                        if (index == 0) { Console.WriteLine("héro inexistant..."); Console.ReadKey(); break; }
+
+                        Console.WriteLine(personnages[index].name);
+                        Console.WriteLine("Dégat 1 = "+personnages[index].GetDegat());
+                        Console.WriteLine("Dégat 2 = " + personnages[index].GetDegat());
+                        Console.ReadKey();
+
                         break;
 
                     case 5:
-                        XmlSerializer serializer2 = new XmlSerializer(typeof(heroes));
-                        StreamReader lecteur = new StreamReader("Test.xml");
-                        heroes hero = (heroes)serializer2.Deserialize(lecteur);
-                        lecteur.Close();
-
+                       
                         break;
 
     //end switch loop for menu selection
